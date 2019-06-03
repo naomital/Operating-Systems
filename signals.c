ad2a5a2150fbdb0d2539f9fa7837796413520654
naomital <naomilau699@gmail.com>
2019-06-03 12:18:07 +0300

    fix and have problem in dead zumby

--- signals.c
+++ signals.c
@@ -7,19 +7,45 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/fcntl.h>
+#include <sys/wait.h>
 
-
+int ppid [5];
+int befor = 3;
+void sigCathcher(int sigin){
+printf("PID %d caught one\n",getpid());
+if(befor>=0){
+kill(ppid[befor],SIGTERM);
+}
+befor--;
+exit(0);
+}
 int main() 
 { 
     for(int i=0;i<5;i++) // loop will run n times (n=5) 
     { 
-        if(fork() == 0) 
+    int temp =fork();
+    //printf("%d\n", temp);
+        if( temp == 0) 
         { 
-            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid()); 
-            exit(0); 
-        } 
+            printf("PID %d ready\n",getpid()); 
+          if(signal(SIGTERM, sigCathcher)== SIG_ERR){
+             printf("signal error");
+         }
+                      pause();
+        }
+     ppid[i]=temp;
     } 
-    for(int i=0;i<5;i++) // loop will run n times (n=5) 
-    wait(NULL); 
-      
+       sleep(1);
+      kill(ppid[4],SIGTERM);
+      sleep(1);
+if(getpid!=0){
+for(int i=0;i<5;i++){
+     sleep(1);
+    kill(ppid[i],SIGKILL);
+    printf("%d is dead\n",ppid[i]);
 }
+}
+
+    return 0;
+      
+};
